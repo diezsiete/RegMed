6cc119d7353df4a2d00292162a43f54326144ce3
Jose Dario Guerrero Aragon <guerrerojosedario@yahoo.com>
2018-04-13 17:08:21 -0500

    Gitignore config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.fundacionjuanpabloii.co';
-//$config['base_url'] = 'http://localhost/RegMed/public_html/';
+//$config['base_url'] = 'http://www.fundacionjuanpabloii.co';
+$config['base_url'] = 'http://localhost/RegMed/public_html/';
 
 /*
 |--------------------------------------------------------------------------
